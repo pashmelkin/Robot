77a7ea013ce4ac98ece9becdf6085e2d2c225cdf
Pavel Shmueli <Pavel.Shmueli@myob.com>
2021-02-01 21:11:14 +1100

    Small rework

--- src/Processor/Processor.ts
+++ src/Processor/Processor.ts
@@ -1,13 +1,13 @@
-
+import {MoveRobot} from "./MoveRobot";
 
 interface IProcessor {
-
+    MoveRobot: () => any
 
 };
 
 
 const Processor: IProcessor = {
-
+    MoveRobot : MoveRobot
 };
 
 export default Processor;
--- src/Repository/Repository.ts
+++ src/Repository/Repository.ts
@@ -1,13 +1,15 @@
-
+import {GetLocation} from './GetLocation';
+import {SetLocation} from './SetLocation';
 
 interface IRepository {
-
-
+    GetLocation: () => any
+    Move: () => any
 };
 
 
 const Repository: IRepository = {
-
+    GetLocation: GetLocation,
+    Move: SetLocation
 };
 
 export default Repository;
