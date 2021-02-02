c551ae3dd70c29c5359498426bd25c5a1ed5bc46
Pavel Shmueli <Pavel.Shmueli@myob.com>
2021-02-03 10:41:42 +1100

    Connect Input and Processor layers

--- src/robotApp.ts
+++ src/robotApp.ts
@@ -1,17 +1,31 @@
 import InputReader from "./Input/InputReader";
 import Sanitizer from "./Input/Sanitizer";
+import {IProcessor} from "./Processor/IProcessor";
+import {Processor} from "./Processor/Processor";
+import {IRepository} from "./Repository/IRepository";
 
 class RobotApp {
 
     private readonly prompt: string = "What is the robot command, please: => ";
-    private inputReader: InputReader;
+    private readonly exitCommand = "EXIT";
+    private readonly inputReader: InputReader;
+    private readonly moveProcessor: IProcessor;
+    private readonly repository : IRepository;
+
     constructor() {
         this.inputReader = new InputReader(this.prompt, new Sanitizer());
+        this.moveProcessor = new Processor(this.repository);
     }
 
     start() {
-        let res = this.inputReader.read();
-        console.log("Read: " + res );
+
+        while(true) {
+            let command = this.inputReader.read ();
+            if (command.includes(this.exitCommand)){
+                break;
+            }
+            this.moveProcessor.MoveRobot (command);
+        }
     }
 }
 
