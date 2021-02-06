588be884af4f717122c9c134071b9a942b6c04b9
Pavel Shmueli <Pavel.Shmueli@myob.com>
2021-02-06 23:05:49 +1100

    Regroup Processor tests

--- src/__tests__/Unit/Processor/Processor.test.ts
+++ src/__tests__/Unit/Processor/Processor.test.ts
@@ -17,25 +17,13 @@ describe("'Processor returns error if no Place command initially issued", () => 
 });
 
 describe('Processor', function () {
-    it('Processor function returns correct location after correct place command', function () {
+    it('MoveRobot function returns correct location after correct place command', function () {
         const repo = new Repository(correctLocation);
         const processor = new Processor(repo);
         const result = processor.MoveRobot('REPORT');
         expect(result).toBe(correctLocationStr);
     });
-});
-
-describe('Processor', function () {
-    it('Processor function returns error message after wrong place command', function () {
-        const repo = new Repository(correctLocation);
-        const processor = new Processor(repo);
-        const resultMove = processor.MoveRobot('PLACE 1 NORTH NORTH');
-        expect(resultMove).toBe('Wrong Place command parameter: NORTH');
-    });
-});
-
-describe('Processor', function () {
-    it('Processor function returns does not change location after wrong place command', function () {
+    it('MoveRobot function returns does not change location after wrong place command', function () {
         const repo = new Repository(correctLocation);
         const processor = new Processor(repo);
         const resultMove = processor.MoveRobot('PLACE 1 NORTH NORTH');
@@ -43,10 +31,7 @@ describe('Processor', function () {
         const resultReport = processor.MoveRobot('REPORT');
         expect(resultReport).toBe(correctLocationStr);
     });
-});
-
-describe('Processor', function () {
-    it('Processor function returns correct location after correct place command', function () {
+    it('MoveRobot function returns correct location after correct place command', function () {
         const repo = new Repository(undefined);
         const processor = new Processor(repo);
         processor.MoveRobot('PLACE 1 2 SOUTH');
