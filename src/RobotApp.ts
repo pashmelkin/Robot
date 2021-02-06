dea3ff742bc6f7bd970e80d65b25641025baa1a9
Pavel Shmueli <Pavel.Shmueli@myob.com>
2021-02-06 21:54:53 +1100

    Add help

--- src/RobotApp.ts
+++ src/RobotApp.ts
@@ -17,8 +17,23 @@ class RobotApp {
         this.boardConfiguration = new BoardConfiguration();
     }
 
+    private help() {
+        console.log("=======================================================");
+        console.log('Welcome to Toy Robot CLI.');
+        console.log("The first command is expected is 'PLACE/'");
+        console.log('PLACE X,Y,F  X,Y - numbers, F is one of directions: North, West, East, South');
+        console.log('Others correct commands are:');
+        console.log('LEFT');
+        console.log('RIGHT');
+        console.log('MOVE');
+        console.log('REPORT');
+        console.log('All commands are case insensitive');
+        console.log("=======================================================");
+
+    }
     start() {
         let result: string = undefined;
+        this.help();
         while (true) {
             const command = this.inputReader.read();
 
