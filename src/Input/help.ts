620b3b50c44f5d592860336a7304ade033adfdf3
Pavel Shmueli <Pavel.Shmueli@myob.com>
2021-02-11 09:27:06 +1100

    Help is dynamic

--- src/Input/help.ts
+++ src/Input/help.ts
@@ -1,13 +1,19 @@
+import { SimpleCommands } from '../models/Commands';
+import { BoardSides } from '../models/BoardSides';
+
 export function help() {
+    let allSides = '';
     console.log('=======================================================');
     console.log('Welcome to Toy Robot CLI.');
     console.log("The first command is expected to be 'PLACE'");
-    console.log('PLACE X,Y,F  X,Y - numbers, F is one of directions: North, West, East, South');
+    for (const side in BoardSides) {
+        allSides += side + ' ';
+    }
+    console.log(`PLACE X,Y,F  X,Y - numbers, F is one of directions: ${allSides}`);
     console.log('Others correct commands are:');
-    console.log('LEFT');
-    console.log('RIGHT');
-    console.log('MOVE');
-    console.log('REPORT');
-    console.log('All commands are case insensitive');
+    for (const cmd in SimpleCommands) {
+        console.log(cmd);
+    }
+    console.log('All commands are accepted in any case');
     console.log('=======================================================');
 }
